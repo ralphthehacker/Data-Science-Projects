3f984b1e2b9b67dae0a3d09d4ee15b357b4430e5
Ralph Blanes <rblanes31@gmail.com>
2016-01-09 14:26:01 -0200

    Started testing and created a Jupyter Notebook

---
+++ Crime Visualizations/test.py
@@ -0,0 +1,6 @@
+import pandas
+
+# Loading data into the 2 dataframes
+seattle_df = pandas.DataFrame.from_csv("/Users/ralphblanes/PycharmProjects/Data-Science-Projects/Crime Visualizations/seattle_incidents_summer_2014.csv")
+sanfran_df = pandas.DataFrame.from_csv("/Users/ralphblanes/PycharmProjects/Data-Science-Projects/Crime Visualizations/sanfrancisco_incidents_summer_2014.csv")
+
